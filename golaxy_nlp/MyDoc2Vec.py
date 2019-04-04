19ed306a517a2fa05e902c94e129594afef4eeab
lihao <imagelihao@163.com>
2019-04-04 14:14:07 +0800

    删除无用py数据

--- golaxy_nlp/MyDoc2Vec.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-"""
-Author LiHao
-Time 2018/12/20 14:01
-"""
-import gensim
-from gensim.models.doc2vec import Doc2Vec,LabeledSentence
-from gensim.models.word2vec import Word2Vec,LineSentence
